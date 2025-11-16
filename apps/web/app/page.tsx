4ae9a82fec88e88708a73632d97da55d59fe1251
gokul1108 <gokul2q@gmail.com>
2025-11-16 22:22:25 +0530

    Added-nginx-conf

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,22 +1,6 @@
-import Image, { type ImageProps } from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
-type Props = Omit<ImageProps, "src"> & {
-  srcLight: string;
-  srcDark: string;
-};
+
 import prisma from '@repo/db/client';
 
-const ThemeImage = (props: Props) => {
-  const { srcLight, srcDark, ...rest } = props;
-
-  return (
-    <>
-      <Image {...rest} src={srcLight} className="imgLight" />
-      <Image {...rest} src={srcDark} className="imgDark" />
-    </>
-  );
-};
 
 export default async function Home() {
   const user = await prisma.user.findFirst();
