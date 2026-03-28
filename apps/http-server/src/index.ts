ada7a51922afc63dcda2a09fd68fe2bd8a102164
gokul1108 <gokul2q@gmail.com>
2026-03-28 23:30:34 +0530

    added booking

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -4,38 +4,192 @@ import prisma from '@repo/db/client';
 const app = express();
 app.use(express.json());
 
-app.post("/signin",async (req,res)=>{
-    const {username,password}=req.body; 
-    try{
-        const user= await prisma.user.findFirst({
-        where:{
-            username,
-            
+app.post("/signin", async (req, res) => {
+    const { email } = req.body;
+
+    if (!email) {
+        return res.status(400).json({ message: "Email is required" });
+    }
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email,
+            }
+        });
+
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
         }
-    });
 
-    if(!user || user.password !== password){
-        return res.status(401).json({message:"Invalid credentials"});
+        return res.json({ 
+            message: "Signin successful",
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name
+            }
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
     }
-    }catch(error){
-        return res.status(500).json({message:"Internal server error"});
-
-    }
-    
-    return res.json({message:"Signin successful"});
-    
 });
+
 app.post("/signup", async (req, res) => {
-    const { username, password } = req.body;
-    await prisma.user.create({
-        data:  {
-            username,
-            password
-             
+    const { email, name } = req.body;
+
+    if (!email || !name) {
+        return res.status(400).json({ message: "Email and name are required" });
+    }
+
+    try {
+        const existingUser = await prisma.user.findUnique({
+            where: { email }
+        });
+
+        if (existingUser) {
+            return res.status(409).json({ message: "User with this email already exists" });
         }
-    });
-    return  res.json({ message: "Signup successful" });
+
+        const user = await prisma.user.create({
+            data: {
+                email,
+                name
+            }
+        });
+
+        return res.status(201).json({ 
+            message: "Signup successful",
+            user: {
+                id: user.id,
+                email: user.email,
+                name: user.name
+            }
+        });
+    } catch (error) {
+        return res.status(500).json({ message: "Internal server error" });
+    }
 });
+
+app.post("/events", async (req, res) => {
+    const { title, location, eventDate, totalSeats } = req.body;
+    if (!title || !location || !eventDate || !totalSeats) {
+        return res.status(400).json({ message: "All event details are required" });
+    }
+    
+
+    const totalSeatsNum = Number(totalSeats);
+    try{
+        const event = await prisma.event.create({
+            data: {
+                title,
+                location,
+                eventDate: new Date(eventDate),
+                totalSeats: totalSeatsNum
+                
+            }
+        });
+        return res.status(201).json({
+            message: "Event created successfully",
+            event: {
+                id: event.id,
+                title: event.title,
+                location: event.location,
+                eventDate: event.eventDate,
+                totalSeats: event.totalSeats,
+                
+            }
+        });
+
+
+
+    }catch(error){
+        return res.status(500).json({ error });
+    }
+    
+
+});
+
+app.post("/bookings", async (req, res) => {
+
+    const { userId, eventId, seats } = req.body;
+    if (!userId || !eventId || !seats) {
+        return res.status(400).json({ message: "All booking details are required" });
+    }
+    try{
+        const booking = await prisma.booking.create({
+            data: {
+                userId,
+                eventId,
+                seats,
+                status: "CONFIRMED"
+                
+            }
+        });
+        return res.status(201).json({
+            message: "Booking created successfully",
+            booking: {
+                id: booking.id,
+                userId: booking.userId,
+                eventId: booking.eventId,
+                seats: booking.seats,
+                
+            }
+        });  
+    }catch(error){
+        return res.status(500).json({ message: "Internal server error" });
+    }   
+});
+
+app.post("/bookings/tx", async (req, res) => {
+    const { userId, eventId, seats } = req.body;
+    if (!userId || !eventId || !seats) {
+        return res.status(400).json({ message: "All booking details are required" });
+    }
+    try {
+        const result = await prisma.$transaction(async(tx)=>{
+            const event = await tx.event.findUnique({
+                where:{
+                    id:eventId
+
+                }
+            });
+            if(!event){
+                throw new Error("Event Not Exist");
+            }
+            if(event.totalSeats<seats){
+                throw new Error("Not Enough Seats");
+            }
+            
+
+            await tx.event.update({
+                where:{
+                    id:eventId,
+
+                },
+                data:{
+                    totalSeats:{
+                        decrement:seats,
+                    }
+                }
+
+            });
+            return tx.booking.create({
+                data:{
+                    userId,
+                    eventId,
+                    seats,
+                    status:"CONFIRMED"
+                }
+            });
+        });
+        return res.status(201).json(result);
+    }catch(error){
+        return res.status(500).json({message: "Internal server error"})
+    }
+
+});
+
 
 app.listen(3002);
 
---
+++ packages/prisma/prisma/migrations/20251219053001_init_schema/migration.sql
@@ -0,0 +1,65 @@
+/*
+  Warnings:
+
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `username` on the `User` table. All the data in the column will be lost.
+  - The `id` column on the `User` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropIndex
+DROP INDEX "User_username_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "password",
+DROP COLUMN "updatedAt",
+DROP COLUMN "username",
+ADD COLUMN     "email" TEXT NOT NULL,
+DROP COLUMN "id",
+ADD COLUMN     "id" SERIAL NOT NULL,
+ALTER COLUMN "name" SET NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    "eventDate" TIMESTAMP(3) NOT NULL,
+    "totalSeats" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Booking" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "eventId" INTEGER NOT NULL,
+    "seats" INTEGER NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Booking_userId_idx" ON "Booking"("userId");
+
+-- CreateIndex
+CREATE INDEX "Booking_eventId_idx" ON "Booking"("eventId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
